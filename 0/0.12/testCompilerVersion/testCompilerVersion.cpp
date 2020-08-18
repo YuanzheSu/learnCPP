5782f14ec41e369c92c74be08810d92260787e36
YuanzheSu <52230751+YuanzheSu@users.noreply.github.com>
2020-08-18 16:58:03 -0500

    Chap 0.12

---
+++ 0/0.12/testCompilerVersion/testCompilerVersion.cpp
@@ -0,0 +1,42 @@
+// testCompilerVersion.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <array>
+#include <iostream>
+#include <string_view>
+#include <tuple>
+#include <type_traits>
+
+namespace a::b::c
+{
+    inline constexpr std::string_view str{ "hello" };
+}
+
+template <class... T>
+std::tuple<std::size_t, std::common_type_t<T...>> sum(T... args)
+{
+    return { sizeof...(T), (args + ...) };
+}
+
+int main()
+{
+    auto [iNumbers, iSum] { sum(1, 2, 3) };
+    std::cout << a::b::c::str << ' ' << iNumbers << ' ' << iSum << '\n';
+
+    std::array arr{ 1, 2, 3 };
+
+    std::cout << std::size(arr) << '\n';
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
